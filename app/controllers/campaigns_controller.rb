1dbf39d61ab6c8e77ddac0ac538055505a6995a2
Jacob Chae <jbcden@gmail.com>
2013-12-28 16:44:22 -0500

    added rest actions to campaign, authorization and custom exceptions

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,11 +1,49 @@
+require_relative '../../lib/exceptions'
 class CampaignsController < ApplicationController
+
+  def show
+    @campaign = Campaign.find(params[:id])
+    raise Exceptions::NotAuthorizedError unless CampaignPolicy.new(current_student, @campaign).authorized?
+  end
+
   def new
     @campaign = Campaign.new
   end
   def create
     @campaign = Campaign.new(params[:campaign])
     @campaign.student_id = current_student.id
-    @campaign.save
+    if @campaign.save
+      flash[:success] = "New campaign created!"
+      redirect_to current_student
+    else
+      flash[:notice] = "You need to fill out all required fields"
+      render 'new'
+    end
+  end
+  
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:id])
+    raise Exceptions::NotAuthorizedError unless CampaignPolicy.new(current_student, @campaign).authorized?
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+    raise Exceptions::NotAuthorizedError unless CampaignPolicy.new(current_student, @campaign).authorized?
+    if @campaign.update_attributes(params[:campaign])
+      flash[:notice] = 'Campaign has been updated.'
+      redirect_to @campaign
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    raise Exceptions::NotAuthorizedError unless CampaignPolicy.new(current_student, @campaign).authorized?
+    @campaign.destroy
     redirect_to current_student
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
+require_relative '../../lib/exceptions'
 class StudentsController < ApplicationController
   def show
-    Student.find(params[:id])
+    @student = Student.find(params[:id])
+    raise Exceptions::NotAuthorizedError unless StudentPolicy.new(@student, current_student).authorized?
   end
 end
