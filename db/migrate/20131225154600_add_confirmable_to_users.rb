fc741672376a5121333a44ab339f357337b11692
Jacob Chae <jbcden@gmail.com>
2013-12-31 16:17:56 -0500

    database issues3

--- db/migrate/20131225154600_add_confirmable_to_users.rb
+++ db/migrate/20131225154600_add_confirmable_to_users.rb
@@ -1,5 +1,6 @@
 class AddConfirmableToUsers < ActiveRecord::Migration
   def self.up
+    create_table :students
     add_column :students, :confirmation_token, :string
     add_column :students, :confirmed_at, :datetime
     add_column :students, :confirmation_sent_at, :datetime
@@ -12,7 +13,7 @@ class AddConfirmableToUsers < ActiveRecord::Migration
     # All existing user accounts should be able to log in after this.
   end
   def self.down
-    remove_columns :students, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    drop_table :students
     # remove_columns :users, :unconfirmed_email # Only if using reconfirmable
   end
 end
