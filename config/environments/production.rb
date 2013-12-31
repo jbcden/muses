6f2393d26cd3f183f7e34e6dbce236065832fe1c
Jacob Chae <jbcden@gmail.com>
2013-12-31 16:48:46 -0500

    fix confirmation mailings1.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Muses::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options[:host] = 'mmuses.org'
+  config.action_mailer.default_url_options = {:host => 'mmuses.org'}
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
