821e44d68447f13ef0a4a689dffbd132e21d9feb
Jacob Chae <jbcden@gmail.com>
2014-01-09 18:19:06 -0500

    fix secret problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+#
+# # Your secret key for verifying the integrity of signed cookies.
+# # If you change this key, all old signed cookies will become invalid!
+# # Make sure the secret is at least 30 characters and all random,
+# # no regular words or you'll be exposed to dictionary attacks.
+Muses::Application.config.secret_token = '57a4572eec6c48cc54fa2845102bffb9481eee12c683d3bb3db1e576878d1b414818a25073fc151656cbb1a6bd746af77e2d09f9fd4ba0d2927cb32d029291aa'
