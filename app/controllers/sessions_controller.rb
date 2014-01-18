1c452babbf6d18633c3bf5cfcb75c3442cc52c6c
Jacob Chae <jbcden@gmail.com>
2014-01-18 17:29:24 -0500

    abstract out session info into private method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,13 @@ class SessionsController < Devise::SessionsController
   # POST /resource/login
   def create
     ps = PolymorphicSessions.new(params).info
-    auth_options = ps[:auth_options]
-    resource_name = ps[:resource_name]
-    warden.config[:default_scope] = ps[:warden]
-    params = ps[:paramaters]
+    session_setup(ps)
     # logger.debug "PS info: #{ps}"
     
-    self.resource = warden.authenticate!(auth_options)
+    self.resource = warden.authenticate!(@auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
-    sign_in(resource_name, resource)
-    if resource_name == :student
+    sign_in(@resource_name, resource)
+    if @resource_name == :student
       redirect_to student_path resource
       else
         respond_with resource, :location => after_sign_in_path_for(resource)
@@ -26,17 +23,17 @@ class SessionsController < Devise::SessionsController
   end
 
   def destroy
-    redirect_path = after_sign_out_path_for(resource_name)
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    redirect_path = after_sign_out_path_for(@resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(@resource_name))
     set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
     redirect_to root_url
   end
 
-  # private
-  #   def session_setup(ps)
-  #   auth_options = ps[:auth_options]
-  #   resource_name = ps[:resource_name]
-  #   warden.config[:default_scope] = ps[:warden]
-  #   params = ps[:paramaters]
-  #   end
+  private
+    def session_setup(ps)
+      @auth_options = ps[:auth_options]
+      @resource_name = ps[:resource_name]
+      warden.config[:default_scope] = ps[:warden]
+      params = ps[:paramaters]
+    end
 end
