8ce60ba41648736396920df5def1d92e81cf5d0b
Jacob Chae <jbcden@gmail.com>
2014-02-10 22:14:32 -0500

    beginnings of the payment system

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -7,4 +7,27 @@ class DonorsController < ApplicationController
   def index
     @messengers = Donor.all
   end
+
+  def donate
+  end
+
+  def payment
+  Stripe.api_key = "sk_test_Zt4bmu85NOFARlheHQNgxD2f"
+
+  # Get the credit card details submitted by the form
+  token = params[:stripeToken]
+
+  # Create a Customer
+  customer = Stripe::Customer.create(
+    :card => token,
+    :account_balance => params[:amount]
+  )
+  save_stripe_customer_id(current_donor, customer.id)
+  redirect_to root_path
+  end
+
+  private
+  def save_stripe_customer_id(donor, customer_id)
+    current_donor.update_attribute(:customer_id, customer_id)
+  end
 end
